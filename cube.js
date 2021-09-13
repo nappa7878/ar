5e87275511ab5922e4d7615f1b025d802b155979
nappa7878 <nappa@kisnet.or.jp>
2021-09-13 11:13:40 +0900

    Add files via upload

--- cube.js
+++ cube.js
@@ -1,5 +1,6 @@
 function modelChange() {
     var p2 = document.getElementById("cube");
+    console.log(p2.style.visibility);
     if (p2.style.visibility == "visible") {
         p2.style.visibility = "hidden";
     } else {
